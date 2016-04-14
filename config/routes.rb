6b53ceb3c5e2ec1fad5c08950f2bb460bdf4e9be
laushinka <laurie.malau@gmail.com>
2016-04-14 19:45:12 +0700

    Generate controller Job

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'job/add'
+
+  get 'job/list'
+
   resources :resumes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
